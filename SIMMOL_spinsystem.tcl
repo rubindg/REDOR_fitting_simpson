04c4ab4d6a27eade3b06ba79efae6bdddb2ce351
Rubin Dasgupta <59262788+rubindg@users.noreply.github.com>
2024-11-23 13:00:22 +0100

    Update SIMMOL_spinsystem.tcl

--- SIMMOL_spinsystem.tcl
+++ SIMMOL_spinsystem.tcl
@@ -6,6 +6,8 @@ mnewspinsys
 set m [mol new 5UZI.pdb first 0 last 0]    # If using NMR structure change the first and last value depending on your requirement
 set m1Ac8 [atomselect $m "resname MA7 and name C8"]                             # C8 from MA7
 set m1Ac8_zone [atomselect $m "within 4 of resname MA7 and name C8"]            # All atoms at ≤4Å of C8 from MA7
-set H_m1Ac8_zone [atomselect $m "index [$m1Ac8_zone list] and name \"H.*"\"]    # Protons at ≤4Å of C8 from MA7
-set C_m1Ac8_zone [atomselect $m "index [$m1Ac8_zone list] and name \"C.*"\"]    # Carbons at ≤4Å of C8 from MA7
-set N_m1Ac8_zone [atomselect $m "index [$m1Ac8_zone list] and name \"N.*"\"]    # Nitrogens at ≤4Å of C8 from MA7
+set H_m1Ac8_zone [atomselect $m "index [$m1Ac8_zone list] and name \"H.*\""]    # Protons at ≤4Å of C8 from MA7
+set C_m1Ac8_zone [atomselect $m "index [$m1Ac8_zone list] and name \"C.*\""]    # Carbons at ≤4Å of C8 from MA7
+set N_m1Ac8_zone [atomselect $m "index [$m1Ac8_zone list] and name \"N.*\""]    # Nitrogens at ≤4Å of C8 from MA7
+set dip_m1Ac8-Call [mdipole $m1Ac8 $C_m1Ac8_zone 0AA 0Hz]
+msavespinsys m1Ac8-Call.spinsys -simpson
